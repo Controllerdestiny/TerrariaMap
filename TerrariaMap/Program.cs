28f5e7af667f4d2435a12bdbef7acdbca7e6cc21
Controllerdestiny <523321293@qq.co>
2024-07-28 14:52:29 +0800

    添加项目文件。

---
+++ TerrariaMap/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace TerrariaMap;
+class Program
+{
+    static void Main(string[] args)
+    { 
+        Console.WriteLine("请输入一个地图路径:");
+        var path = Console.ReadLine();
+        if(!File.Exists(path))
+            throw new FileNotFoundException("找不到指令路径的地图文件!");
+        Console.WriteLine("初始化中..");
+        CreateMapFile.Instance.Init();
+        Console.WriteLine("解析地图内容..");
+        var info = CreateMapFile.Instance.Start(File.ReadAllBytes(path));
+        Console.WriteLine("写入文件...");
+        File.WriteAllBytes(info.Name,info.Buffer);
+        Console.WriteLine($"小地图生成成功:{info.Name}");
+    }
+
+}
+
