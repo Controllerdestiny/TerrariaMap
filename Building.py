1f67dce739c67c5448bf72d0fae6f03ffa10392b
少司命 <79201070+Controllerdestiny@users.noreply.github.com>
2024-07-31 20:49:24 +0800

    Update Building.py

--- Building.py
+++ Building.py
@@ -15,5 +15,6 @@ def zip_files_in_folder(folder_path, zip_file_path):
     
 if __name__ == '__main__':
     build_name = sys.argv[1]
+    print(build_name)
     directory_path = "TerrariaMap/bin/Release/net8.0/publish/"
     zip_files_in_folder(directory_path + build_name, "TerrariaMap" + build_name + ".zip")
